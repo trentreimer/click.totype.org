e1688892f14953959e1f6a5a6ec5dd79b24378ae
trentreimer <48562882+trentreimer@users.noreply.github.com>
2026-09-18 15:47:02 -0500

    Bump suggest-engine to v0.2.2

--- js/language-settings.js
+++ js/language-settings.js
@@ -2,7 +2,7 @@ import { languages } from './languages.js';
 import { settings } from './settings.js';
 import { setKeyboard, applyShift } from './keyboard.js';
 import { loadComposition, compositionIdle } from './composition.js';
-import { SuggestEngine } from 'https://cdn.jsdelivr.net/gh/trentreimer/suggest-engine@v0.2.0/dist/suggest-engine.esm.js';
+import { SuggestEngine } from 'https://cdn.jsdelivr.net/gh/trentreimer/suggest-engine@v0.2.2/dist/suggest-engine.esm.js';
 
 const userWordsStoragePrefix = 'ctt';
 const userWordsEnabledKey = `${userWordsStoragePrefix}:user-words-enabled`;

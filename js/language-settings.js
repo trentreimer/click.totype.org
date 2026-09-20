ef8d1f04eb32b0e052dd1a765b6ce096fc206661
trentreimer <48562882+trentreimer@users.noreply.github.com>
2026-09-19 20:19:21 -0500

    Update to suggest-engine v0.4.0

--- js/language-settings.js
+++ js/language-settings.js
@@ -2,7 +2,7 @@ import { languages } from './languages.js';
 import { settings } from './settings.js';
 import { setKeyboard, applyShift } from './keyboard.js';
 import { loadComposition, compositionIdle } from './composition.js';
-import { SuggestEngine } from 'https://cdn.jsdelivr.net/gh/trentreimer/suggest-engine@v0.3.0/dist/suggest-engine.esm.js';
+import { SuggestEngine } from 'https://cdn.jsdelivr.net/gh/trentreimer/suggest-engine@v0.4.0/dist/suggest-engine.esm.js';
 
 const userWordsStoragePrefix = 'ctt';
 const userWordsEnabledKey = `${userWordsStoragePrefix}:user-words-enabled`;
